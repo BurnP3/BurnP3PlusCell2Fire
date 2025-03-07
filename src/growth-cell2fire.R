6da363526254c16534ca6b87c41db0d58578ce9f
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2025-03-07 11:42:24 -0500

    Support for FBP output and burn perimeter changes
    XML update for consolidating FBP outputs
    
    Update error handling for daily burn perimeters
    
    Support for new FBP spatial output options table

--- src/growth-cell2fire.R
+++ src/growth-cell2fire.R
@@ -63,7 +63,8 @@ GreenUp <- datasheet(myScenario, "burnP3Plus_GreenUp", lookupsAsFactors = F, opt
 Curing <- datasheet(myScenario, "burnP3Plus_Curing", lookupsAsFactors = F, optional = T)
 FuelLoad <- datasheet(myScenario, "burnP3Plus_FuelLoad", lookupsAsFactors = F, optional = T)
 OutputOptions <- datasheet(myScenario, "burnP3Plus_OutputOption", optional = T)
-OutputOptionsSpatial <- datasheet(myScenario, "burnP3Plus_OutputOptionSpatial", optional = T)
+OutputOptionsSpatial <- datasheet(myScenario, "burnP3Plus_OutputOptionSpatial", optional = T) %>% mutate(BurnPerimeter = as.character(BurnPerimeter))
+OutputOptionFBPSpatial <- datasheet(myScenario, "burnP3Plus_OutputOptionFBPSpatial", optional = T, returnInvisible = T) %>% mutate(Variable = as.character(Variable))
 FireZoneTable <- datasheet(myScenario, "burnP3Plus_FireZone")
 WeatherZoneTable <- datasheet(myScenario, "burnP3Plus_WeatherZone")
 
@@ -103,13 +104,15 @@ if(isDatasheetEmpty(OutputOptions)) {
 }
 
 if(isDatasheetEmpty(OutputOptionsSpatial)) {
-  updateRunLog("No spatial output options chosen. Defaulting to keeping all spatial outputs.", type = "info")
+  updateRunLog("No spatial output options chosen. Defaulting to keeping all spatial outputs and final burn perimeters.", type = "info")
   OutputOptionsSpatial[1,] <- rep(TRUE, length(OutputOptionsSpatial[1,]))
+  OutputOptionsSpatial$BurnPerimeter <- "Final"
   saveDatasheet(myScenario, OutputOptionsSpatial, "burnP3Plus_OutputOptionSpatial")
 } else if (any(is.na(OutputOptionsSpatial))) {
   updateRunLog("Missing one or more spatial output options. Defaulting to keeping unspecified spatial outputs.", type = "info")
   OutputOptionsSpatial <- OutputOptionsSpatial %>%
     replace(is.na(.), TRUE)
+  OutputOptionsSpatial$BurnPerimeter <- replace(OutputOptionsSpatial$BurnPerimeter, OutputOptionsSpatial$BurnPerimeter == TRUE, "Final")
   saveDatasheet(myScenario, OutputOptionsSpatial, "burnP3Plus_OutputOptionSpatial")
 }
 
@@ -133,6 +136,10 @@ if(isDatasheetEmpty(WeatherZoneTable))
 # Handle unsupported inputs
 if(!isDatasheetEmpty(WindGrid)) {
   updateRunLog("Cell2Fire currently does not support Wind Grids. Wind Grid options ignored.", type = "info")
+}
+
+if(!isDatasheetEmpty(OutputOptionFBPSpatial)) {
+  updateRunLog("Cell2Fire currently does not support spatial FBP outputs. Spatial FBP output options ignored.", type = "info")
 }
 
 if(!isDatasheetEmpty(GreenUp)) {
@@ -901,7 +908,7 @@ if(OutputOptionsSpatial$AllPerim | (saveBurnMaps & minimumFireSize > 0)){
 
 
 ## Burn perimeters ----
-if(OutputOptionsSpatial$BurnPerimeter) {
+if(OutputOptionsSpatial$BurnPerimeter != "No") {
   updateRunLog("Cell2Fire does not provide burn perimeters.", type = "info")
 }
 
